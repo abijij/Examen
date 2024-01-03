336eeb9089b5d75fd9bdb93d6d729cc23a8d7f69
abijij <105397465+abijij@users.noreply.github.com>
2024-01-03 11:35:57 -0600

    Siguiente Repuesta

--- src/main/java/Pregunta_1/Respuesta_1.java
+++ src/main/java/Pregunta_1/Respuesta_1.java
@@ -1,2 +1,20 @@
-package Pregunta_1;public class Respuesta_1 {
+package Pregunta_1;
+
+
+//Escribe un programa en Java que imprima los números del 1 al 10 en orden ascendente, separados por una coma: 1,2,3,4,5,6,7,8,9,10
+
+public class Respuesta_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.print(i);
+
+
+            if (i < 10) {
+                System.out.print(",");
+            }
+        }
+    }
+
 }
+
---
+++ src/main/java/Pregunta_2/Respuesta_2.java
@@ -0,0 +1,4 @@
+package Pregunta_2;
+
+public class Respuesta_2 {
+}
