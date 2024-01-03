a16eb221968eff0c1527b801dca78f8ee69362f3
abijij <105397465+abijij@users.noreply.github.com>
2024-01-03 12:00:20 -0600

    Pregunta dos finalizada

--- src/main/java/Pregunta_2/Respuesta_2.java
+++ src/main/java/Pregunta_2/Respuesta_2.java
@@ -1,4 +1,30 @@
 package Pregunta_2;
+//¿Cuál es la diferencia entre un for y un while en Java? Proporciona un ejemplo de cada uno.
 
+//Ambos son estructura  de control en Java se utulizan para ejecutar bloques de codigo repetidamente y
+//la diferencia en tre ellos radica en su sintaxis y el momento que evaluan la condicion
 public class Respuesta_2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1 ; i <= 5 ; i++ ) {
+
+            //el for consta de tres partes la inicializacion , la condicion y la actualizacion y se
+            // ejecutara mientras la condicion sea verdadera
+
+            System.out.println("Bucle for = " + i);
+        }
+
+        int i = 1;
+        while (i <= 5) {
+            //El ciclo while solo tiene una sola condicion  el bloque de codigo se ejecutara mientras la condicion sea
+            //verdadera
+            System.out.println("Bucle while = " + i);
+
+            i++;
+            //esto sirve oara evitar un bucle infinito incrementar la variable
+        }
+
+    }
+
 }
